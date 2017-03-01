fc5f70ade484e6e279397cbc7821b1c4bfa2d7b3
xiaokang <874418611@qq.com>
2017-03-01 18:34:15 +0800

    更新

--- src/main/java/com/example/aozun/testapplication/activity/BaseActivity.java
+++ src/main/java/com/example/aozun/testapplication/activity/BaseActivity.java
@@ -9,6 +9,7 @@ import android.view.WindowManager;
 
 /**
  * Created by HHD-H-I-0369 on 2017/1/6.
+ * Activity的基类
  */
 public class BaseActivity extends Activity{
     protected int screenW ,screenH;
@@ -20,18 +21,18 @@ public class BaseActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);//禁止截屏
-        getScreenSize();//获取屏幕宽高
+        getScreenSize();
         applicationShared=getSharedPreferences("applicationshared",MODE_PRIVATE);
     }
-
+    //获取屏幕的相关信息
     public void getScreenSize(){
         WindowManager wm= (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics displayMetrics=new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(displayMetrics);
         screenH=displayMetrics.heightPixels;
         screenW=displayMetrics.widthPixels;
-        s_density=displayMetrics.density;
-        s_dendityDpi=displayMetrics.densityDpi;
+        s_density=displayMetrics.density;//屏幕密度(0.75/1.0/1.5)
+        s_dendityDpi=displayMetrics.densityDpi;//屏幕密度dpi(120/160/240)
         //screenW = getResources().getDisplayMetrics().widthPixels;
         //screenH = getResources().getDisplayMetrics().heightPixels;
 
